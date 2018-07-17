1c9e730c88059dad4734edeccb11394ddf389eac
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-07-16 23:39:07 -0300

    make splash page

--- spearheadsite/urls.py
+++ spearheadsite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from spearheadapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
